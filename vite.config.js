103895f797a6ea148f6bd8f1cd4b818c254cffcf
Emekus-Kora <kwasidebb007@yahoo.com>
2025-06-06 02:23:06 +0100

    `Update base path in Vite configuration`

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'Sonia-clone',  // <-- add this line
+  base: '/Sonia-clone/',  // <-- add this line
   plugins: [react()],
 });
